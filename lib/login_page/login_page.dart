bf51501082504fbb9818e2b800ed76bb2fb328b1
Anil Antony <anilantony49@gmail.com>
2026-02-24 13:24:46 +0530

    Login UI completed

---
+++ lib/login_page/login_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_assesment/login_page/widgets/signin_field_widget.dart';
+import 'package:flutter_assesment/login_page/widgets/signin_widgets.dart';
+ 
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      extendBodyBehindAppBar: true,
+      appBar: CustomAppbar.show(context, false, 'Login'),
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SignInFieldWidget(),
+                Positioned(
+                  bottom: 0,
+                  child: SignInWidgets.signUpNavigate(context),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login_page/widgets/signin_widgets.dart
@@ -0,0 +1,35 @@
+ import 'package:flutter/material.dart';
+import 'package:flutter_assesment/utils/alerts_and_navigators.dart';
+ 
+
+class SignInWidgets {
+  static InkWell signUpNavigate(BuildContext context) {
+    return InkWell(
+      onTap: () => nextScreen(context, const RegistrationPage()),
+      child: FadeInUp(
+        delay: const Duration(milliseconds: 700),
+        duration: const Duration(milliseconds: 1000),
+        child: Text.rich(
+          textAlign: TextAlign.center,
+          TextSpan(
+            children: [
+              TextSpan(
+                text: "Don't have an account? ",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              TextSpan(
+                text: 'Register.',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
